836090893acc8ab9af9854e4e93f8b90c0a99c5f
Jason Evans <jason.evans37@admiralgroup.co.uk>
2018-12-12 14:57:04 +0000

    Initial commit

---
+++ WebAssert/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAssert.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string imagePath = "")
+        {
+            WebAssert.Assert(() => !string.IsNullOrEmpty(imagePath), "The param 'imagePath' must not be empty.");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAssert/WebAssert.cs
@@ -0,0 +1,91 @@
+﻿namespace WebAssert
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using System.Web;
+
+    public static class WebAssert
+    {
+        [Conditional("DEBUG")]
+        public static void Assert(
+            Func<bool> assertCondition,
+            string failedAssertMessage = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = 0,
+            [CallerMemberName] string callerMember = "")
+        {
+            if (assertCondition())
+            {
+                return;
+            }
+
+            //
+            // As the assertion has failed, let's check if we are attached to a debugger. If we are, then break into it.
+            // That way we give the developer a chance to view the current state of the app i.e. by navigating the call stack.
+            //
+            // If we are not attached to a debugger, then lets output the failed assertion details to the current http context,
+            // via a HTML file template.
+            //
+            if (Debugger.IsAttached)
+            {
+                //
+                // Give the developer a head start by outputting the failed assert message to the 'Immediate Window'.
+                //
+                Debug.WriteLine(string.Empty);
+                Debug.WriteLine(string.Empty);
+                Debug.WriteLine($"******************** Assert Failed ({DateTime.Now:dd/MM/yyyy HH:mm:ss tt}) ********************");
+                Debug.WriteLine($"Message: {failedAssertMessage}");
+
+                Debugger.Break();
+            }
+            else
+            {
+                RenderFailedAssertDetails(
+                    failedAssertMessage,
+                    callerFilePath,
+                    callerLineNumber,
+                    callerMember);
+            }
+        }
+
+        private static void RenderFailedAssertDetails(
+            string failedAssertMessage,
+            string callerFilePath,
+            int callerLineNumber,
+            string callerMember)
+        {
+            HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            HttpContext.Current.Response.ContentType = "text/html; charset=utf-8";
+            HttpContext.Current.Response.Write(
+                LoadFailedAssertViewFileHtml()
+                    .Replace("{{AssertMessage}}", failedAssertMessage)
+                    .Replace("{{MethodName}}", callerMember)
+                    .Replace("{{FilePath}}", callerFilePath)
+                    .Replace("{{LineNumber}}", callerLineNumber.ToString())
+            );
+
+            HttpContext.Current.Response.End();
+        }
+
+        private static string LoadFailedAssertViewFileHtml()
+        {
+            try
+            {
+                string templatePath = $@"{HttpContext.Current.Request.PhysicalApplicationPath}\ViewTemplates";
+
+                return File.ReadAllText(templatePath + "\\AssertFailed.cshtml");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(string.Empty);
+                Debug.WriteLine(string.Empty);
+                Debug.WriteLine($"******************** WebAssert.LoadFailedAssertViewFileHtml Error ({DateTime.Now:dd/MM/yyyy HH:mm:ss tt}) ********************");
+                Debug.WriteLine(ex.ToString());
+
+                return string.Empty;
+            }
+        }
+    }
+}
